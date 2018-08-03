1c6e76a3b0ed2576d93f8d494271702856cbac51
colmexdev <github@colmex.mx>
2018-08-03 11:32:28 -0500

    Integración frontend primer aproximación

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require jquery.remotipart
 //= require bootstrap.min
+//= require functions
 //= require bootstrap-datepicker
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.es.js

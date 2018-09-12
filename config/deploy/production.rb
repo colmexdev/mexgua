8ffb99271fea55fbe9bba8e352322a4ffcf39f6b
colmexdev <github@colmex.mx>
2018-09-12 12:01:53 -0500

    Corrección HTML

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ set :stage, :production
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'mexico-guatemala-desarrollo.colmex.mx', user: 'webuser', roles: %w{web app db}
+server 'mexico-guatemala.colmex.mx', user: 'webuser', roles: %w{web app db}
 
 
 # role-based syntax
